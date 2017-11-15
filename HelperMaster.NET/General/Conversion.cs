e78422a355e5391c9a207e3a90abb2a8b934f5d5
calaluis <calaluis@gmail.com>
2017-11-14 23:41:20 -0300

    Se añade método de conversión.

--- HelperMaster.NET/General/Conversion.cs
+++ HelperMaster.NET/General/Conversion.cs
@@ -8,5 +8,14 @@ namespace HelperMaster.NET.General
 {
     public class Conversion
     {
+        public static decimal StringToDecimal(string Dato, decimal ValorDefecto = 0)
+        {
+            decimal TempDecimal = 0;
+            if(decimal.TryParse(Dato, out TempDecimal))
+            {
+                return decimal.Parse(Dato);
+            }
+            return ValorDefecto;
+        }
     }
 }
