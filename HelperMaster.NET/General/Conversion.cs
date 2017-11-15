b666265b6233a2ea6ad95b6f67148ff9808888d3
calaluis <calaluis@gmail.com>
2017-11-14 23:48:29 -0300

    Se añade documentación al método.

--- HelperMaster.NET/General/Conversion.cs
+++ HelperMaster.NET/General/Conversion.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace HelperMaster.NET.General
 {
+    /// <summary>
+    /// Clase que define los métodos de conversión de datos complementarios a los que 
+    /// trae .NET Framework.
+    /// </summary>
     public class Conversion
     {
+        /// <summary>
+        /// Método que permite convertir desde un string a un decimal.
+        /// </summary>
+        /// <param name="Dato">El dato de tipo string a convertir.</param>
+        /// <param name="ValorDefecto">El valor por defecto en caso que la conversión falle. 
+        /// Por defecto es cero.</param>
+        /// <returns></returns>
         public static decimal StringToDecimal(string Dato, decimal ValorDefecto = 0)
         {
             decimal TempDecimal = 0;
