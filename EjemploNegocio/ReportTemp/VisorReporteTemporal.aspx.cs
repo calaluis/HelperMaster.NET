75e782b92f3e6bca515851b585a079a683980dae
calaluis <calaluis@gmail.com>
2018-06-12 18:30:42 -0400

    Se agrega código de ejemplo demostrativo (Falta por construir).

---
+++ EjemploNegocio/ReportTemp/VisorReporteTemporal.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelperMaster.NET.MVC
+{
+    public partial class VisorReporteTemporal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                HtmlHelperMVC.Reporte.ID = VisorReporte.ID;
+
+                VisorReporte.ProcessingMode = HtmlHelperMVC.Reporte.ProcessingMode;
+                VisorReporte.SizeToReportContent = HtmlHelperMVC.Reporte.SizeToReportContent;
+                VisorReporte.Width = HtmlHelperMVC.Reporte.Width;
+                VisorReporte.Height = HtmlHelperMVC.Reporte.Height;
+                VisorReporte.LocalReport.ReportPath = HtmlHelperMVC.Reporte.LocalReport.ReportPath;
+                VisorReporte.LocalReport.DataSources.Clear();
+                foreach (var item in HtmlHelperMVC.Reporte.LocalReport.DataSources)
+                {
+                    VisorReporte.LocalReport.DataSources.Add(item);
+                }
+                VisorReporte.LocalReport.Refresh();
+            }
+        }
+    }
+}
---
+++ EjemploNegocio/Reporteria/Libreria/Reporte.cs
@@ -0,0 +1,24 @@
+﻿using HelperMaster.NET.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploNegocio.Reporteria.Libreria
+{
+    /// <summary>
+    /// Clase que define la usabilidad de generar reportes en archivos PDF.
+    /// </summary>
+    public partial class Reporte
+    {
+        /// <summary>
+        /// Atributo que guarda o establece los datos del archivo de plantilla en iTextXML.
+        /// </summary>
+        public string DatosPlantilla { get; set; }
+        /// <summary>
+        /// Atributo que guarda o establece el motor de reporteria.
+        /// </summary>
+        private PDF P { get; set; }
+    }
+}
