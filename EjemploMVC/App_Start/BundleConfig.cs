8dbdcc553e081d74603df053ad3a63ecc00e90d2
calaluis <calaluis@gmail.com>
2018-06-18 12:59:10 -0400

    Se crea instancia inicial de código de ejemplo. Falta desarrollarlo.

---
+++ EjemploMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace EjemploMVC.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/Content/bootstraptheme").Include("~/Content/bootstrap-theme.css"));
+        }
+    }
+}
--- EjemploNegocio/Reporteria/Libreria/Reporte.cs
+++ EjemploNegocio/Reporteria/Libreria/Reporte.cs
@@ -1,9 +1,7 @@
-﻿using HelperMaster.NET.General;
+﻿using HelperMaster.NET.DTO;
+using HelperMaster.NET.Enum;
+using HelperMaster.NET.General;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EjemploNegocio.Reporteria.Libreria
 {
@@ -13,12 +11,42 @@ namespace EjemploNegocio.Reporteria.Libreria
     public partial class Reporte
     {
         /// <summary>
-        /// Atributo que guarda o establece los datos del archivo de plantilla en iTextXML.
-        /// </summary>
-        public string DatosPlantilla { get; set; }
-        /// <summary>
         /// Atributo que guarda o establece el motor de reporteria.
         /// </summary>
         private PDF P { get; set; }
+        /// <summary>
+        /// Método de ejemplo que permite generar reporte en PDF.
+        /// </summary>
+        /// <param name="Test">Cadena de entrada con fines docentes.</param>
+        /// <returns>TRUE = Reporte construido sin problemas; FALSE = Problemas al consultar el reporte.</returns>
+        public Respuesta<byte[]> ConsultarReporte(string Test)
+        {
+            Respuesta<byte[]> Resp = new Respuesta<byte[]>();
+
+            var ObtPlantilla = Helper.ConsultarArchivoDesdeEnsamblado("ReporteEjemplo.html");
+            if (!ObtPlantilla.decision)
+            { return Resp.MensajeGenerico(); }
+
+            this.P = new PDF(ObtPlantilla.estructuraDatos);
+            this.P.PlantillaPDF = this.P.PlantillaPDF.Replace("[FechaCreacion]", DateTime.Now.ToString());
+            var Imagen = Helper.ConsultarArchivoDesdeEnsamblado("001_hola_mundo.jpg");
+            if (!Imagen.decision)
+            { return Resp.MensajeGenerico(); }
+
+            this.P.PlantillaPDF = this.P.PlantillaPDF.Replace("[IMAGEN]", "data:image/jpg;base64," + Convert.ToBase64String(Imagen.estructuraDatos));
+            this.P.PlantillaPDF = this.P.PlantillaPDF.Replace("[TEST]", "Reporte de ejemplo que demuestra la usabilidad " +
+                "de otorger reportería a un sistema de información administrativo.");
+
+            this.P.GenerarPDF();
+
+            Resp.estructuraDatos = this.P.ArchivoPDF;
+
+            Resp.decision = true;
+            Resp.mensaje = "Completar Reporte.";
+            Resp.mensajeDetalle = "Reporte construido sin problemas.";
+            Resp.TipoRespuesta = TipoRespuesta.CorrectoNegocio;
+
+            return Resp;
+        }
     }
 }
